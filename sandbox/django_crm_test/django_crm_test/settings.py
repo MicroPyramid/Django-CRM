b2c425be8168d24fd53ecb46e58de3f1d5f1d9fd
anjaneyulu <anjaneyulu.batta@micropyramid.com>
2017-06-08 17:54:24 +0530

    settings updated

--- sandbox/django_crm_test/django_crm_test/settings.py
+++ sandbox/django_crm_test/django_crm_test/settings.py
@@ -123,10 +123,10 @@ ALLOWED_HOSTS = ['*']
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+# # Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (
+#     os.path.join(PROJECT_ROOT, 'static'),
+# )
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
