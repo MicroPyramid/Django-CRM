111f8c56091ec77d072befb79e61ae5e8a99d02f
nikhila05 <nikhila@micropyramid.com>
2018-01-02 11:58:26 +0530

    updated changes for heroku

--- crm/settings.py
+++ crm/settings.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -73,11 +74,11 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+   'default': dj_database_url.config(
+       default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
+   )
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
