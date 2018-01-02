2619b1774e600c2c489ee45ecbb8f428497b44ec
nikhila05 <nikhila@micropyramid.com>
2018-01-02 12:03:59 +0530

    updated changes for heroku

--- crm/settings.py
+++ crm/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'mwx@&97%!$fx_*zgj(2ygi^(s=oh5j(cqb$=+-mkd9scbt!0v0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
