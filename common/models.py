bc6aebb250f62094df1b15fe1e44b63e041e7c6c
nikhila05 <nikhila@micropyramid.com>
2018-10-12 15:02:01 +0530

    changed bread crumb in all update pages, displaying address properly in all view pages

--- common/models.py
+++ common/models.py
@@ -48,6 +48,38 @@ class Address(models.Model):
     def __str__(self):
         return self.city if self.city else ""
 
+    def get_complete_address(self):
+        address = ""
+        if self.address_line:
+            address += self.address_line
+        if self.street:
+            if address:
+                address += ", " + self.street
+            else:
+                address += self.street
+        if self.city:
+            if address:
+                address += ", " + self.city
+            else:
+                address += self.city
+        if self.state:
+            if address:
+                address += ", " + self.state
+            else:
+                address += self.state
+        if self.postcode:
+            if address:
+                address += ", " + self.postcode
+            else:
+                address += self.postcode
+        if self.country:
+            if address:
+                address += ", " + self.get_country_display()
+            else:
+                address += self.get_country_display()
+        return address
+
+
 
 class Team(models.Model):
     name = models.CharField(max_length=55)
