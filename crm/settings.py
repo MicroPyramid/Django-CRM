1b20863155df1e784fac8328c45c193b9cb7b4d0
kika115 <kristinka.kostova@gmail.com>
2018-12-17 07:52:32 +0100

    oprava postgres

--- crm/settings.py
+++ crm/settings.py
@@ -73,7 +73,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'dj_crm',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': '5432'
     }
