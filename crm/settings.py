3f82df460c3e5e93ea178fa8494476795463b8af
nikhila05 <nikhila@micropyramid.com>
2018-12-26 21:57:20 +0530

    updated debug to true fro production environments

--- crm/settings.py
+++ crm/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'mwx@&97%!$fx_*zgj(2ygi^(s=oh5j(cqb$=+-mkd9scbt!0v0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
