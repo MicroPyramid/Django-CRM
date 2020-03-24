5cdd47734c9b60b537777a0cea3486395bb5c4f4
Anand Tiwari <anandtiwarics@gmail.com>
2020-03-24 14:00:46 +0530

    fixed Cross-site Request Forgery (CSRF) Vulnerability

--- crm/settings.py
+++ crm/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware'
 ]
 
 ROOT_URLCONF = 'crm.urls'
