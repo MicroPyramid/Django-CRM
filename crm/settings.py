2592d7649f45c2c40d826ab10bbe0f30588b749a
Jessica Patnode <jess.patnode1@gmail.com>
2020-04-03 20:09:56 -0700

    settings file

--- crm/settings.py
+++ crm/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
