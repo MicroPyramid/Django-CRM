aa40904a0dd0a241c42d8856bfef614db34e27f8
Hanson Hu <>
2020-04-06 19:24:45 +0800

    customizable application title

--- crm/settings.py
+++ crm/settings.py
@@ -68,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django_settings_export.settings_export',
             ],
         },
     },
