a47a44a49e7d55df25ccf974b04668bb95df001e
Ashwin Kumar <ashwin@micropyramid.com>
2021-10-06 17:41:22 +0530

    new release (#452)

--- crm/urls.py
+++ crm/urls.py
@@ -42,7 +42,9 @@ urlpatterns = [
     path("logout/", views.LogoutView, {"next_page": "/login/"}, name="logout"),
 ]
 
+
 if settings.DEBUG:
     urlpatterns = (
-        urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+        urlpatterns
+        + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     )
