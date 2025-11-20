51ab094f08afd2ebfc1a02ce2155673e00b6de7c
Ashwin <ashwin@micropyramid.com>
2025-11-20 17:20:22 +0530

    refactor: remove unused backend scripts and team-related files
    - Deleted entrypoint.sh and wait-for-postgres.sh as they are no longer needed.
    - Removed gunicorn.sh and migrate.sh scripts to streamline deployment.
    - Cleaned up teams app by deleting models, serializers, views, migrations, and URLs related to teams.
    - Added new board detail page and task management functionality in the frontend.

--- backend/boards/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class BoardsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'boards'
-    verbose_name = 'Kanban Boards'
--- backend/docker/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Execute startup scripts
-./wait-for-postgres.sh "$DBHOST"
-python3 manage.py collectstatic --noinput
-python3 manage.py migrate
-python3 manage.py runserver 0.0.0.0:8000
