f7418fdc005ca2b6d6ae31cb145b126f2e9587a0
Ashwin <ashwin@micropyramid.com>
2026-02-20 18:01:58 +0530

    chore: Remove Sentry example page and its corresponding API route.

--- frontend/src/routes/sentry-example-page/+server.js
+++
@@ -1,6 +0,0 @@
-// This is just a very simple API route that throws an example error.
-// Feel free to delete this file and the entire sentry route.
-
-export const GET = async () => {
-  throw new Error("Sentry Example API Route Error");
-};
